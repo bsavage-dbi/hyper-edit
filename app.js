0e34e6d7beb0cd1499b115d49d2e595327181edc
kwaimind <danieel.reed@gmail.com>
2019-07-11 13:24:12 +0200

    prettify

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 var hasRun = false;
 var adminUrl;
 window.onload = function() {
-	if (!hasRun) {
-		var regex = /"pageId":"(.*?)"/g;
-		var found = document.body.innerHTML.match(regex);
-		if (found) {
-			var clean = found[0].replace("pageId", "");
-			var adminUrl = clean.replace(/[":]/g, "");
-			console.log("%c Hyper Wordpress edit link 👇", "background: #222; color: #bada55");
-			console.log("🌍 " + "https://hyperisland.wpengine.com/wp-admin/post.php?post=" + adminUrl + "&action=edit&lang=en");
-			hasRun = true;
-		};
-	}
+    if (!hasRun) {
+        var regex = /"pageId":"(.*?)"/g;
+        var found = document.body.innerHTML.match(regex);
+        if (found) {
+            var clean = found[0].replace("pageId", "");
+            var adminUrl = clean.replace(/[":]/g, "");
+            console.log("%c Hyper Wordpress edit link 👇", "background: #222; color: #bada55");
+            console.log("🌍 " + "https://hyperisland.wpengine.com/wp-admin/post.php?post=" + adminUrl + "&action=edit&lang=en");
+            hasRun = true;
+        };
+    }
 }
