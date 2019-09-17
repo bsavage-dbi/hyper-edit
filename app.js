ab9cbba4cc67cff07a0bd42e5e9268a7cb295270
kwaimind <danieel.reed@gmail.com>
2019-09-17 17:29:16 +0200

    adding fallback messaging for disallowed urls

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
 document.addEventListener(
 	'DOMContentLoaded',
 	function() {
+		let hasRun = false;
+		let tabUrl;
 		chrome.tabs.query({ currentWindow: true, active: true }, function(tabs) {
 			chrome.tabs.sendMessage(tabs[0].id, 'hi', getPageId);
+			tabUrl = tabs[0].url;
 		});
-		let hasRun = false;
 		function getPageId(res) {
-			const buttonLink = document.getElementById('url');
-			buttonLink.setAttribute(
-				'href',
-				`https://hyperisland.wpengine.com/wp-admin/post.php?post=${res.adminUrl}&action=edit&lang=en`
-			);
+			if (!tabUrl.includes('hyperisland.com')) {
+				document.getElementById('heythere').textContent =
+					"Oh no. You're not on a Hyper Island web page 😔";
+				document.getElementById('intro').style.display = 'none';
+				document.getElementById('button-container').style.display = 'none';
+			} else {
+				const buttonLink = document.getElementById('url');
+				buttonLink.setAttribute(
+					'href',
+					`https://hyperisland.wpengine.com/wp-admin/post.php?post=${res.adminUrl}&action=edit&lang=en`
+				);
+			}
 			hasRun = true;
 		}
 	},
--- background.js
+++ background.js
@@ -1,14 +1,9 @@
 //this file is not finished and not used in production
 
 //chrome.browserAction.setIcon({path: icon});
-chrome.tabs.query(
-	{
-		active: true,
-		currentWindow: true
-	},
-	function(tabs) {
-		var tab = tabs[0];
-		var url = tab.url;
-		chrome.browserAction.setBadgeText({ text: url });
+chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+	var currTab = tabs[0];
+	if (currTab) {
+		console.log(window.location);
 	}
-);
+});
