96ac9d309dab67995228c9eabf18a1233670a61d
kwaimind <danieel.reed@gmail.com>
2019-09-19 08:58:21 +0200

    greater page type coverage
    covers more page types. cleaner regex on clean.

--- content.js
+++ content.js
@@ -1,6 +1,6 @@
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-	const regex = /\"\$ROOT\_QUERY\.pages\.(locked|default|blogPost)\(\{\\"id\\":(\d+)/g;
-	const replace = /\$ROOT\_QUERY\.pages\.(locked|default|blogPost)\(\{\\"id\\"/g;
+	const regex = /\"\$ROOT\_QUERY\.pages\.(locked|default|blogPost|eventPost|program|course)\(\{\\"id\\":(\d+)/g;
+	const replace = /\$ROOT\_QUERY\.pages\.(.*?)\(\{\\"id\\"/g;
 	const found = document.body.innerHTML.match(regex);
 	const clean = found[0].replace(replace, '');
 	const adminUrl = clean.replace(/[":]/g, '');
